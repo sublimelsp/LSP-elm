c6c4d487bb1c7a83daab850f4de516eb72dd2151
Predrag <idmpepe@gmail.com>
2020-03-27 00:32:04 +0100

    Update LSP-elm to use lsp-utils

--- plugin.py
+++ plugin.py
@@ -1,105 +1,73 @@
 import shutil
 import os
 import sublime
-import threading
-import subprocess
 
 from LSP.plugin.core.handlers import LanguageHandler
-from LSP.plugin.core.settings import ClientConfig, LanguageConfig, read_client_config
+from LSP.plugin.core.settings import ClientConfig, read_client_config
 
+from lsp_utils import ServerNpmResource
 
-package_path = os.path.dirname(__file__)
-server_path = os.path.join(package_path, 'node_modules', '@elm-tooling', 'elm-language-server', 'out', 'index.js')
+PACKAGE_NAME = 'LSP-elm'
+SETTINGS_FILENAME = 'LSP-elm.sublime-settings'
+SERVER_DIRECTORY = 'server'
+SERVER_BINARY_PATH = os.path.join(SERVER_DIRECTORY, 'node_modules', '@elm-tooling', 'elm-language-server', 'out', 'index.js')
+
+server = ServerNpmResource(PACKAGE_NAME, SERVER_DIRECTORY, SERVER_BINARY_PATH)
 
 
 def plugin_loaded():
-    is_server_installed = os.path.isfile(server_path)
-    print('LSP-elm: Server {} installed.'.format('is' if is_server_installed else 'is not' ))
-
-    # install if not installed
-    if not is_server_installed:
-        # this will be called only when the plugin gets:
-        # - installed for the first time,
-        # - or when updated on package control
-        logAndShowMessage('LSP-elm: Installing server.')
-
-        runCommand(
-            onCommandDone,
-            ["npm", "install", "--verbose", "--prefix", package_path, package_path]
-        )
+    server.setup()
 
 
-def onCommandDone():
-    logAndShowMessage('LSP-elm: Server installed.')
-
-
-def runCommand(onExit, popenArgs):
-    """
-    Runs the given args in a subprocess.Popen, and then calls the function
-    onExit when the subprocess completes.
-    onExit is a callable object, and popenArgs is a list/tuple of args that
-    would give to subprocess.Popen.
-    """
-    def runInThread(onExit, popenArgs):
-        try:
-            if sublime.platform() == 'windows':
-                subprocess.check_call(popenArgs, shell=True)
-            else:
-                subprocess.check_call(popenArgs)
-            onExit()
-        except subprocess.CalledProcessError as error:
-            logAndShowMessage('LSP-elm: Error while installing the server.', error)
-        return
-    thread = threading.Thread(target=runInThread, args=(onExit, popenArgs))
-    thread.start()
-    # returns immediately after the thread starts
-    return thread
+def plugin_unloaded():
+    server.cleanup()
 
 
 def is_node_installed():
     return shutil.which('node') is not None
 
 
-def logAndShowMessage(msg, additional_logs=None):
-    print(msg, '\n', additional_logs) if additional_logs else print(msg)
-    sublime.active_window().status_message(msg)
-
-
 class LspElmPlugin(LanguageHandler):
     @property
     def name(self) -> str:
-        return 'lsp-elm'
+        return PACKAGE_NAME.lower()
 
     @property
     def config(self) -> ClientConfig:
-        settings = sublime.load_settings("LSP-elm.sublime-settings")
-        client_configuration = settings.get('client')
+        # Calling setup() also here as this might run before `plugin_loaded`.
+        # Will be a no-op if already ran.
+        # See https://github.com/sublimelsp/LSP/issues/899
+        server.setup()
 
-        default_configuration = {
+        settings = sublime.load_settings(SETTINGS_FILENAME)
+
+        configuration = {
+            "enabled": settings.get("enabled", True),
             "command": [
                 'node',
-                server_path,
+                server.binary_path,
                 '--stdio'
             ],
-            "languages": [
+            "languages": settings.get("languages", [
                 {
                     "languageId": "elm",
                     "scopes": ["source.elm"],
                     "syntaxes": ["Packages/Elm Syntax Highlighting/src/elm.sublime-syntax"]
                 }
-            ],
-            "initializationOptions": {
+            ]),
+            "initializationOptions": settings.get("initializationOptions", {
                 "elmAnalyseTrigger": "change"
-            }
+            }),
+            "settings": settings.get("settings", {})
         }
-        default_configuration.update(client_configuration)
-        return read_client_config('lsp-elm', default_configuration)
+
+        return read_client_config('lsp-elm', configuration)
 
     def on_start(self, window) -> bool:
         if not is_node_installed():
-            sublime.status_message('Please install Node.js for the Elm Language Server to work.')
+            sublime.status_message("{}: Please install Node.js for the server to work.".format(PACKAGE_NAME))
             return False
-        return True
+        return server.ready
 
     def on_initialized(self, client) -> None:
         pass   # extra initialization here.
