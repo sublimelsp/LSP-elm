e0319f0010bda0fb95d7e5c4576e7896073f8af4
Предраг Николић <idmpepe@gmail.com>
2022-10-20 22:57:34 +0200

    handle show references code lens
    closes #40

--- plugin.py
+++ plugin.py
@@ -1,6 +1,9 @@
-import os
+from .types import ShowReferencesAction
+from LSP.plugin.core.typing import Mapping, Callable, Any, cast
+from LSP.plugin.locationpicker import LocationPicker
 from lsp_utils import NpmClientHandler
-
+import os
+import sublime
 
 def plugin_loaded():
     LspElmPlugin.setup()
@@ -20,3 +23,29 @@ class LspElmPlugin(NpmClientHandler):
     @classmethod
     def required_node_version(cls) -> str:
         return ">=14"
+
+    def on_pre_server_command(self, params: Mapping[str, Any], done_callback: Callable[[], None]) -> bool:
+        if params['command'] == 'editor.action.showReferences':
+            arguments = cast(ShowReferencesAction, params['arguments'])
+            session = self.weaksession()
+            if not session:
+                return False
+            view = sublime.active_window().active_view()
+            if not view:
+                return False
+            references = arguments[0]['references']
+            if len(references) == 1:
+                args = {
+                    'location': references[0],
+                    'session_name': session.config.name,
+                }
+                view.run_command('lsp_open_location', args)
+            elif references:
+                LocationPicker(view, session, references, side_by_side=False)
+            else:
+                sublime.status_message('No references found')
+            done_callback()
+        return True
+
+
+
