afb16c53b9c747ca11bd2ce5f52430a97999c59c
Предраг Николић <idmpepe@gmail.com>
2022-10-21 01:00:17 +0200

    implement move function code action
    closes #49

--- plugin.py
+++ plugin.py
@@ -1,4 +1,5 @@
-from .types import ShowReferencesParams
+from LSP.plugin.core.protocol import Dict, Request, Response
+from .types import MaybeMoveFunctionCodeAction, MoveDestinationsResponse, MoveParamsParams, ShowReferencesParams, MoveParams
 from LSP.plugin.core.typing import Mapping, Callable, Any, cast
 from LSP.plugin.locationpicker import LocationPicker
 from lsp_utils import NpmClientHandler
@@ -46,5 +47,56 @@ class LspElmPlugin(NpmClientHandler):
             done_callback()
         return True
 
+    def on_server_response_async(self, method: str, response: Response) -> None:
+        if method == 'codeAction/resolve':
+            self.maybe_handle_move_code_action(response)
 
+    def maybe_handle_move_code_action(self, response: Response) -> None:
+        session = self.weaksession()
+        if not session:
+            return
+        result = cast(MaybeMoveFunctionCodeAction, response.result)
+        command = result.get('command')
+        if not command:
+            return
+        command_name = command.get('arguments')[0]
+        params = command.get('arguments')[1]
+        function_name = command.get('arguments')[2]
+        if command_name == 'moveFunction':
+            move_params = {
+                'sourceUri': params.get('textDocument').get('uri'),
+                'params': params
+            }  # type: MoveParams
+            get_move_destinations_request = Request('elm/getMoveDestinations', move_params)
+            session.send_request(get_move_destinations_request, lambda res: self.on_get_destinations(res, params, function_name))
 
+    def on_get_destinations(self, response: MoveDestinationsResponse, params: MoveParamsParams, function_name: str) -> None:
+        destinations = response.get('destinations')
+        if not destinations:
+            sublime.status_message('LSP-elm: No destinations to choose.')
+        session = self.weaksession()
+        if not session:
+            return
+        window = sublime.active_window()
+        if not window:
+            return
+
+        def on_done(index) -> None:
+            if index == -1:
+                return
+            destination = destinations[index]
+            move_params = {
+                'sourceUri': params.get('textDocument').get('uri'),
+                'params': params,
+                'destination': destination
+            }  # type: MoveParams
+            move_request = Request("elm/move", move_params)
+            session.send_request(move_request, self.on_move_function)
+
+        placeholder = 'Select the new file for the function {}'.format(function_name)
+        items = [d['name'] for d in destinations]
+        window.show_quick_panel(items, placeholder=placeholder, on_select=on_done)
+
+    def on_move_function(self, _: Any) -> None:
+        # There is nothing to be done anymore
+        return None
--- types.py
+++ types.py
@@ -1,6 +1,7 @@
-from LSP.plugin.core.protocol import URI, Location, Range
-from LSP.plugin.core.typing import TypedDict, List
+from LSP.plugin.core.protocol import URI, CodeActionParams, Location, NotRequired, Range, TextDocumentIdentifier
+from LSP.plugin.core.typing import Tuple, TypedDict, List
 
+# Code Lens Show Reference - Start
 ShowReference = TypedDict('ShowReference', {
     'references': List[Location],
     'uri': URI,
@@ -8,3 +9,40 @@ ShowReference = TypedDict('ShowReference', {
 })
 
 ShowReferencesParams = List[ShowReference]
+# Code Lens Show Reference - End
+
+# Move Function Code Action - Start
+MoveDestination = TypedDict('MoveDestination', {
+  'name': str,
+  'path': str,
+  'uri': str
+})
+
+MoveParamsParams = TypedDict('MoveFunctionCodeActionArgument', {
+    'textDocument': TextDocumentIdentifier,
+    'range': Range
+})
+
+MoveParams = TypedDict('MoveParams', {
+  'sourceUri': URI,
+  'params': MoveParamsParams,
+  'destination': NotRequired[MoveDestination]
+})
+
+MoveDestinationsResponse = TypedDict('MoveDestinationsResponse', {
+  'destinations': List[MoveDestination]
+})
+
+MoveFunctionCodeAction = TypedDict("MoveFunctionCodeAction", {
+  'arguments': Tuple[str, MoveParamsParams, str],
+  'command': str,  # string like elm.refactor-${workspaceId}, for example: 'elm.refactor-file:///home/predragnikolic/Documents/sandbox/elm-spa-example'
+  'title': str
+})
+
+MaybeMoveFunctionCodeAction = TypedDict("MaybeMoveFunctionCodeAction", {
+  'command': NotRequired[MoveFunctionCodeAction]
+  # other properties are not important for the move request
+})
+# Move Function Code Action - End
+
+
