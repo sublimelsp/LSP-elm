4fe290d444b5471dbfa0343af476ae0568ea0ed0
Предраг Николић <idmpepe@gmail.com>
2023-11-19 18:54:33 +0100

    use correct path

--- plugin.py
+++ plugin.py
@@ -19,7 +19,7 @@ class LspElmPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'server'
     server_binary_path = os.path.join(
-        server_directory, 'node_modules', '@elm-tooling', 'elm-language-server', 'out', 'index.js'
+        server_directory, 'node_modules', '@elm-tooling', 'elm-language-server', 'out', 'node', 'index.js'
     )
 
     @classmethod
