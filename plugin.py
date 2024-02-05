b80536a1f526c807a25e7744efbe04a96df4ba3c
Предраг Николић <idmpepe@gmail.com>
2024-02-05 20:03:04 +0100

    make server work in ST3

--- plugin.py
+++ plugin.py
@@ -14,5 +14,5 @@ class LspElmPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'server'
     server_binary_path = os.path.join(
-        server_directory, 'node_modules', '@elm-tooling', 'elm-language-server', 'out', 'index.js'
+        server_directory, 'node_modules', '@elm-tooling', 'elm-language-server', 'out', 'node', 'index.js'
     )
