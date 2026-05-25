12ba2cd0a420c11e0ce74871fd5e7faac01b3a07
Rafal Chlodnicki <rchl2k@gmail.com>
2026-05-25 13:04:54 +0200

    refactor: migrate to LspPlugin

--- types.py
+++ types.py
@@ -1,24 +1,12 @@
 from __future__ import annotations
 
-from LSP.protocol import Location
+from LSP.protocol import DocumentUri
 from LSP.protocol import Range
 from LSP.protocol import TextDocumentIdentifier
-from LSP.protocol import URI
-from typing import List
 from typing import TypedDict
 from typing_extensions import NotRequired
 
 
-# Code Lens Show Reference - Start
-class ShowReference(TypedDict):
-    references: list[Location]
-    uri: URI
-    range: Range
-
-ShowReferencesParams = List[ShowReference]
-# Code Lens Show Reference - End
-
-# Move Function Code Action - Start
 class MoveDestination(TypedDict):
     name: str
     path: str
@@ -31,7 +19,7 @@ class MoveParamsParams(TypedDict):
 
 
 class MoveParams(TypedDict):
-    sourceUri: URI
+    sourceUri: DocumentUri
     params: MoveParamsParams
     destination: NotRequired[MoveDestination]
 
@@ -44,4 +32,3 @@ class MoveFunctionCommand(TypedDict):
     arguments: tuple[str, MoveParamsParams, str]
     command: str  # string like elm.refactor-${workspaceId}, for example: 'elm.refactor-file:///home/predragnikolic/Documents/sandbox/elm-spa-example'
     title: str
-# Move Function Code Action - End
