c956f719fc2358456128a3aeaa55d442cdbb1863
Rafał Chłodnicki <rchl2k@gmail.com>
2026-05-29 21:31:30 +0200

    refactor: migrate to LspPlugin (#71)

--- plugin.py
+++ plugin.py
@@ -4,71 +4,51 @@ from .types import MoveDestinationsResponse
 from .types import MoveFunctionCommand
 from .types import MoveParams
 from .types import MoveParamsParams
-from .types import ShowReferencesParams
-from collections.abc import Mapping
-from LSP.plugin.core.protocol import Request
-from LSP.plugin.core.protocol import Response
-from LSP.plugin.locationpicker import LocationPicker
+from LSP.plugin import Error
+from LSP.plugin import LspPlugin
+from LSP.plugin import OnPreStartContext
+from LSP.plugin import Request
+from LSP.plugin import ServerResponse
+from LSP.plugin import Session
 from LSP.protocol import CodeAction
-from lsp_utils import NpmClientHandler
-from typing import Any
-from typing import Callable
+from lsp_utils import NodeManager
+from pathlib import Path
+from sublime_lib import ResourcePath
 from typing import cast
-import os
+from typing_extensions import override
 import sublime
 
 
 def plugin_loaded():
-    LspElmPlugin.setup()
+    LspElmPlugin.register()
 
 
 def plugin_unloaded():
-    LspElmPlugin.cleanup()
+    LspElmPlugin.unregister()
 
 
-class LspElmPlugin(NpmClientHandler):
-    package_name = str(__package__)
-    server_directory = 'server'
-    server_binary_path = os.path.join(
-        server_directory, 'node_modules', '@elm-tooling', 'elm-language-server', 'out', 'node', 'index.js'
-    )
+class LspElmPlugin(LspPlugin):
 
     @classmethod
-    def required_node_version(cls) -> str:
-        return ">=14"
+    @override
+    def on_pre_start_async(cls, context: OnPreStartContext) -> None:
+        package_name = cls.plugin_storage_path.name
+        NodeManager.on_pre_start_async(
+            context,
+            cls.plugin_storage_path,
+            ResourcePath('Packages', package_name, 'server'),
+            Path('node_modules', '@elm-tooling', 'elm-language-server', 'out', 'node', 'index.js'),
+            node_version_requirement='>=14',
+        )
 
-    def on_pre_server_command(self, params: Mapping[str, Any], done_callback: Callable[[], None]) -> bool:
-        if params['command'] == 'editor.action.showReferences':
-            arguments = cast(ShowReferencesParams, params['arguments'])
-            session = self.weaksession()
-            view = sublime.active_window().active_view()
-            if not session or not view:
-                done_callback()
-                return True
-            references = arguments[0]['references']
-            if len(references) == 1:
-                args = {
-                    'location': references[0],
-                    'session_name': session.config.name,
-                }
-                view.run_command('lsp_open_location', args)
-            elif references:
-                LocationPicker(view, session, references, side_by_side=False)
-            else:
-                sublime.status_message('No references found')
-            done_callback()
-        return True
+    @override
+    def on_server_response_async(self, response: ServerResponse) -> None:
+        if response['method'] == 'codeAction/resolve' and (session := self.weaksession()):
+            code_action = response['result']
+            if code_action['title'] != 'Move Function':
+                self.handle_move_code_action(session, code_action)
 
-    def on_server_response_async(self, method: str, response: Response) -> None:
-        if method == 'codeAction/resolve':
-            self.maybe_handle_move_code_action(cast(CodeAction, response.result))
-
-    def maybe_handle_move_code_action(self, code_action: CodeAction) -> None:
-        if code_action['title'] != 'Move Function':
-            return
-        session = self.weaksession()
-        if not session:
-            return
+    def handle_move_code_action(self, session: Session, code_action: CodeAction) -> None:
         command = cast(MoveFunctionCommand, code_action.get('command'))
         if not command:
             return
@@ -77,12 +57,14 @@ class LspElmPlugin(NpmClientHandler):
             'sourceUri': params.get('textDocument').get('uri'),
             'params': params
         }
-        session.send_request(Request('elm/getMoveDestinations', move_params),
-                             lambda res: self.on_get_destinations(res, params, function_name))
+        session.send_request_task(Request('elm/getMoveDestinations', move_params)) \
+            .then(lambda result: self.on_get_destinations(result, params, function_name))
 
     def on_get_destinations(
-        self, response: MoveDestinationsResponse, params: MoveParamsParams, function_name: str
+        self, response: MoveDestinationsResponse | Error, params: MoveParamsParams, function_name: str
     ) -> None:
+        if isinstance(response, Error):
+            return
         destinations = response.get('destinations')
         if not destinations:
             sublime.status_message('LSP-elm: No destinations to choose.')
@@ -102,9 +84,8 @@ class LspElmPlugin(NpmClientHandler):
                 'params': params,
                 'destination': destination
             }
-            session.send_request(Request("elm/move", move_params),
-                                 lambda _: None)  # no need to handle result
+            session.send_request_task(Request("elm/move", move_params))  # no need to handle result
 
-        placeholder = 'Select the new file for the function {}'.format(function_name)
+        placeholder = f'Select the new file for the function {function_name}'
         items = [d['name'] for d in destinations]
         window.show_quick_panel(items, placeholder=placeholder, on_select=on_done)
